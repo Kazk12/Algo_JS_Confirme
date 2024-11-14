e4151a6fccc3b4339cfe38fd73cfd4c265c53da0
Kazk12 <kazuko43KO@gmail.com>
2024-11-14 13:48:14 +0100

    DONE : exercice 4 et 5

---
+++ js/exercice4.js
@@ -0,0 +1,9 @@
+// const tab = ["jérémy", "hassan", "vincent"];
+// let nom = "";
+// let times = 1;
+
+// for(let i = 0; i < tab.length; i += 1){
+//     nom = tab[i] + " ";
+//     console.log(nom.repeat(times));
+//     times++;
+// }
---
+++ js/exercice5.js
@@ -0,0 +1,14 @@
+// Exercice 5.1
+
+// while(true){
+//     const nombre = 1;
+//     const UserGuess = window.prompt(`Entrez un nombre entre 1 et 3 :`);
+//     if((UserGuess > 3) || (UserGuess < 1)){
+//         window.alert(`Veuillez entrez un bon nombre`);
+//         continue;
+//     }
+//     else if (UserGuess == nombre){
+//         window.alert(`Bravo vous avez saisie le bon nombre`);
+//         break;
+//     } ;
+// }
