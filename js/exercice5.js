58a511497feacc2fca2e634cdde30e6a0d737cbe
Kazk12 <kazuko43KO@gmail.com>
2024-11-14 15:02:28 +0100

    DONE : Exercice 5.3 5.4

--- js/exercice5.js
+++ js/exercice5.js
@@ -12,3 +12,42 @@
 //         break;
 //     } ;
 // }
+
+
+
+
+// Exercice 5.2
+
+
+// while(true){
+//     const nombre = 13;
+//     const UserGuess = window.prompt(`Entrez un nombre entre 10 et 20 :`);
+//     if (UserGuess == nombre){
+//         window.alert(`Bravo vous avez saisie le bon nombre`);
+//         break;
+//     } ;
+
+//     if((UserGuess > 20)){
+//         window.alert(`Veuillez entrez un nombre plus petit`);
+//     } else if((UserGuess < 10)){
+//         window.alert(`Veuillez entrez un nombre plus grand`);
+//     }
+//     else {
+//         window.alert(`Dommage ce n'est pas le bon nombre`);
+//     }
+    
+// }
+
+
+
+
+// Exercice 5.3  //     5.4
+
+
+// let num = Number(prompt(`Entrez un nombre ici :`));
+
+// for(let i = 0; i < 10; i +=1){
+    
+//     num += 1;
+//     console.log(num);
+// };
